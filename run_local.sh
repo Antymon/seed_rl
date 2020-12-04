7bd075a877bd7fa2ff4e31e51e5166df279c2d13
szymon <s.brych@instadeep.com>
2020-12-04 11:17:04 +0100

    Accept cuda visibility and alter container name

--- run_local.sh
+++ run_local.sh
@@ -21,21 +21,27 @@ die () {
 
 ENVIRONMENTS="atari|dmlab|football"
 AGENTS="r2d2|vtrace|sac"
-[ "$#" -ne 0 ] || die "Usage: run_local.sh [$ENVIRONMENTS] [$AGENTS] [Num. actors]"
+[ "$#" -ne 0 ] || die "Usage: run_local.sh [$ENVIRONMENTS] [$AGENTS] [Num. actors] [[CUDA_VISIBLE_DEVICES]]"
 echo $1 | grep -E -q $ENVIRONMENTS || die "Supported games: $ENVIRONMENTS"
 echo $2 | grep -E -q $AGENTS || die "Supported agents: $AGENTS"
 echo $3 | grep -E -q "^((0|([1-9][0-9]*))|(0x[0-9a-fA-F]+))$" || die "Number of actors should be a non-negative integer without leading zeros"
 export ENVIRONMENT=$1
 export AGENT=$2
 export NUM_ACTORS=$3
+
+if [ -z "$4" ]; then
+  die "CUDA_VISIBLE_DEVICES not provided"
+fi
+
 export CONFIG=$ENVIRONMENT
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd $DIR
 docker/build.sh
 docker_version=$(docker version --format '{{.Server.Version}}')
+DOCKER_PARAMS="-e CUDA_VISIBLE_DEVICES=$4 --entrypoint ./docker/run.sh -ti -it --name szy_seed_original  --rm seed_rl:$ENVIRONMENT $ENVIRONMENT $AGENT $NUM_ACTORS"
 if [[ "19.03" > $docker_version ]]; then
-  docker run --entrypoint ./docker/run.sh -ti -it --name seed --rm seed_rl:$ENVIRONMENT $ENVIRONMENT $AGENT $NUM_ACTORS
+  eval "docker run $DOCKER_PARAMS"
 else
-  docker run --gpus all --entrypoint ./docker/run.sh -ti -it -e HOST_PERMS="$(id -u):$(id -g)" --name seed --rm seed_rl:$ENVIRONMENT $ENVIRONMENT $AGENT $NUM_ACTORS
+  eval "docker run --gpus all -e HOST_PERMS="$(id -u):$(id -g)" $DOCKER_PARAMS"
 fi
