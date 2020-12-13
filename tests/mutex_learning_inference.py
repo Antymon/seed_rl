641151fee93c83bcbc2460b5997602bf5559a8bc
szymon <s.brych@instadeep.com>
2020-12-13 16:46:01 +0100

    Adding bunch of artificial dependencies to prevent autograph from (re)moving things that compromise synchronization

--- tests/mutex_learning_inference.py
+++ tests/mutex_learning_inference.py
@@ -35,6 +35,8 @@ def learner_loop(model):
 
         if training:
             model.train()
+        else:
+            model.simulate_work(5e5)
 
         print("{} {}".format("tick" if i % 2 == 0 else "tock", i))
         time.sleep(1)
@@ -50,15 +52,7 @@ class DummyModel(tf.Module):
     def train(self):
         raise NotImplementedError()
 
-
-class MutexDummyModel(DummyModel):
-
-    def __init__(self):
-        print("MutexDummyModel")
-        self.is_training = tf.Variable(initial_value=False, trainable=False, dtype=tf.bool)
-        self.is_inferring = tf.Variable(initial_value=tf.zeros([len(ACTOR_NAMES)], dtype=tf.bool), trainable=False,
-                                        dtype=tf.bool)
-
+    @tf.function
     def simulate_work(self, steps, prints_count=0, on_count_msg=None, on_finish_fn=None):
         condition = lambda i: tf.less(i, int(steps))
 
@@ -74,6 +68,23 @@ class MutexDummyModel(DummyModel):
             on_finish_fn()
 
         return result
+
+    @tf.function
+    def busy_wait(self, id, condition_fn, print_freq=1e5, ):
+        wait_cycles = tf.constant(0)
+        while condition_fn():
+            if wait_cycles % int(print_freq) == 0:
+                tf.print(id,"waiting", wait_cycles)
+            wait_cycles += 1
+        return wait_cycles
+
+class MutexDummyModel(DummyModel):
+
+    def __init__(self):
+        print("MutexDummyModel")
+        self.is_training = tf.Variable(initial_value=False, trainable=False, dtype=tf.bool)
+        self.is_inferring = tf.Variable(initial_value=tf.zeros([len(ACTOR_NAMES)], dtype=tf.bool), trainable=False,
+                                        dtype=tf.bool)
 
     @tf.function(input_signature=[tf.TensorSpec([], tf.int32, 'env_id'), ])
     def infer(self, env_id):
@@ -94,7 +105,7 @@ class MutexDummyModel(DummyModel):
             self.is_inferring[env_id].assign(False)
             tf.print(env_id, "done", self.is_inferring[env_id])
 
-        r = self.simulate_work(5e5, 10, ("working", env_id), on_finish_fn)
+        r = self.simulate_work(3e5, 10, ("working", env_id), on_finish_fn)
 
         return env_id, r
 
@@ -107,7 +118,6 @@ class MutexDummyModel(DummyModel):
             if wait_cycles % int(1e5) == 0:
                 tf.print("training waiting", wait_cycles, self.is_inferring)
             wait_cycles += 1
-
 
         tf.print("training start", wait_cycles, self.is_training)
 
@@ -131,16 +141,22 @@ class MutexViaInferenceInvalidationDummyModel(DummyModel):
         # if violated, inference outcome is unknown and likely to contribute noise
         # do inference only if training has not started and accept result only if training is not on when returning
         current_inference_invalid = tf.constant(True)
+        r = tf.constant(0)
         while current_inference_invalid:
-            while self.is_training:
-                pass
-            # imitate work
-            for _ in range(int(1e4)):
-                pass
+            wait_cycles = self.busy_wait(env_id, lambda: self.is_training)
+            tf.print(env_id, "waited", wait_cycles, self.is_training)
 
-            current_inference_invalid = self.is_training
+            r = self.simulate_work(3e5, 3, ("working", env_id))[0]
 
-        return env_id
+            # dummy condition, always true to prevent optimization
+            if r >= tf.constant(0):
+                current_inference_invalid = self.is_training
+                if current_inference_invalid:
+                    tf.print(env_id, "inference INVALIDATED")
+                else:
+                    tf.print(env_id, "done")
+
+        return env_id, r
 
     @tf.function
     def train(self):
@@ -148,12 +164,11 @@ class MutexViaInferenceInvalidationDummyModel(DummyModel):
 
         tf.print("training start")
 
-        for _ in range(int(1e5)):
-            pass
+        def on_finish_fn():
+            self.is_training.assign(False)
+            tf.print("training end", self.is_training)
 
-        tf.print("training end")
-
-        self.is_training.assign(False)
+        self.simulate_work(5e5, 10, "training", on_finish_fn)
 
         return
 
@@ -178,5 +193,5 @@ def run_experiment(model):
 
 
 if __name__ == '__main__':
-    run_experiment(MutexDummyModel())
-    # run_experiment(MutexViaInferenceInvalidationDummyModel())
+    # run_experiment(MutexDummyModel())
+    run_experiment(MutexViaInferenceInvalidationDummyModel())
